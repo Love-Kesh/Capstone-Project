da19748cc10b4841f7a5cd78c66f837477cb4646
Lovekesh <lovekeshparihar121@gmail.com>
2025-02-10 18:58:14 -0700

    Added the fading launch screen

--- app/src/main/java/com/example/capstoneproject/MainActivity.kt
+++ app/src/main/java/com/example/capstoneproject/MainActivity.kt
@@ -4,56 +4,83 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
-import com.example.capstoneproject.ui.theme.CapstoneProjectTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CapstoneProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppContent()
         }
     }
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ){
-        Text(
-            text = "Dating App!!",
-            fontSize = 30.sp
-        )
+fun AppContent() {
+    var showSplash by remember { mutableStateOf(true) }
+    var visible by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        delay(2000)
+        visible = false
+        delay(500) //delay for the fade out animation
+        showSplash = false
     }
 
+    AnimatedVisibility(
+        visible = showSplash,
+        exit = fadeOut(animationSpec = tween(durationMillis = 500))
+    ) {
+        LaunchScreen()
+    }
+
+    AnimatedVisibility(
+        visible = !showSplash,
+        enter = fadeIn(animationSpec = tween(durationMillis = 500))
+    ) {
+        LoginScreen()
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CapstoneProjectTheme {
-        Greeting()
+fun LaunchScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black), //background color
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Dating App!!",
+            fontSize = 30.sp,
+            color = Color.White
+        )
+    }
+}
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Login Screen", fontSize = 24.sp)
     }
 }
